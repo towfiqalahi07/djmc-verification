42b07d1e906f3870a5b33839e397fad2c4cbe89d
Md Towfiq Alahi <towfiq.ytpro@gmail.com>
2026-03-09 15:08:07 +0600

    Update GOOGLE_SCRIPT_URL in config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   // Replace with your deployed Google Apps Script web app URL.
-  GOOGLE_SCRIPT_URL: "",
+  GOOGLE_SCRIPT_URL: "https://github.com/towfiqalahi07/djmc-verification",
 };
